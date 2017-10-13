897a651b9fd1535ff8aaaf0996ac6af52f643947
Ewan Barr <ewan.d.barr@googlemail.com>
2017-10-13 15:25:15 +0200

    added scripts

--- scripts/loki_queue_consumer_example.py
+++ scripts/loki_queue_consumer_example.py
@@ -1,12 +1,12 @@
 import sys
 from loki.broker.queue import Queue, RedisQueueEngine
 
-redis_engine = RedisQueueEngine('redis',6379)
+redis_engine = RedisQueueEngine('127.0.0.1',6379)
 input_queue = Queue('examples::data_consumer::input', redis_engine)
 output_queue = Queue('examples::data_consumer::output', redis_engine)
 while True:
     data = input_queue.rpop(blocking=True)
     print "From input queue:",data
     sys.stdout.flush()
-    output_queue.lpush('Got one')
+    output_queue.lpush(data)
     
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
+import glob
 
 setup(name='loki',
       version='0.1',
-      packages=find_packages()
+      packages=find_packages(),
+      scripts=glob.glob("scripts/*.py")
       )
