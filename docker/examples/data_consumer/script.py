5632f97f234a2822ecdca1e8808132a9b4bd471c
Ewan Barr <ewan.d.barr@googlemail.com>
2017-10-13 14:10:09 +0200

    added basic setup

---
+++ docker/examples/data_consumer/script.py
@@ -0,0 +1,9 @@
+from loki.broker.queue import Queue, RedisQueueEngine
+
+redis_engine = RedisQueueEngine('redis',6379)
+input_queue = Queue('examples::data_consumer::input', redis_engine)
+output_queue = Queue('examples::data_consumer::output', redis_engine)
+while True:
+    data = input_queue.rpop()
+    print "From input queue:",data
+    output_queue.lpush(data)
