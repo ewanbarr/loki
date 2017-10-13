99870b994aabb1e693f73e09061b970778476e77
Ewan Barr <ewan.d.barr@googlemail.com>
2017-10-13 17:03:51 +0200

    script updates

--- scripts/loki_queue_consumer_example.py
+++ scripts/loki_queue_consumer_example.py
@@ -4,10 +4,12 @@ from loki.broker.queue import Queue, RedisQueueEngine
 
 host = sys.argv[1]
 port = sys.argv[2]
+print "Connecting to Redis"
 redis_engine = RedisQueueEngine(host,port)
 input_queue = Queue('examples::data_consumer::input', redis_engine)
 output_queue = Queue('examples::data_consumer::output', redis_engine)
 while True:
+    print "Acquiring from queue '{}'".format('examples::data_consumer::input')
     data = input_queue.rpop(blocking=True)
     print "From input queue:",data
     sys.stdout.flush()
--- scripts/loki_queue_producer_example.py
+++ scripts/loki_queue_producer_example.py
@@ -1,13 +1,17 @@
 import sys
+import time
 from loki.broker.queue import Queue, RedisQueueEngine
 
 host = sys.argv[1]
 port = sys.argv[2]
+print "Connecting to Redis"
 redis_engine = RedisQueueEngine(host,port)
 input_queue = Queue('examples::data_consumer::input', redis_engine)
 ii = 0
 while True:
     data = "Message no: {}".format(ii)
+    print "Pushing: '{}' to queue '{}'".format(data,'examples::data_consumer::input')
     input_queue.lpush(data)
     ii+=1
+    time.sleep(5)
     
